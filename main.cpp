bd1b180446ac996e80a6f38a655bf178b91244c4
Mihaske <163606090+Mihaske@users.noreply.github.com>
2025-07-24 13:20:44 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,8 +14,84 @@ path operator""_p(const char* data, std::size_t sz) {
     return path(data, data + sz);
 }
 
-// напишите эту функцию
-bool Preprocess(const path& in_file, const path& out_file, const vector<path>& include_directories);
+bool ProcessIncludes(istream& input, ostream& output, const path& file_path, const vector<path>& include_directories, int& line_number);
+
+bool Preprocess(const path& in_file, const path& out_file, const vector<path>& include_directories) {
+    ifstream input(in_file);
+    if (!input.is_open()) {
+        cout << "unknown include file " << in_file.string() << " at file " << in_file.string() << " at line 1" << endl;
+        return false;
+    }
+
+    ofstream output(out_file);
+    if (!output.is_open()) {
+        return false;
+    }
+
+    int line_number = 1;
+    return ProcessIncludes(input, output, in_file, include_directories, line_number);
+}
+
+bool ProcessIncludes(istream& input, ostream& output, const path& file_path, const vector<path>& include_directories, int& line_number) {
+    static const regex include_local(R"~(^\s*#\s*include\s*\"([^\"]*)\"\s*$)~");
+    static const regex include_system(R"~(^\s*#\s*include\s*\<([^\>]*)\>\s*$)~");
+
+    string line;
+    while (getline(input, line)) {
+        smatch match;
+        if (regex_match(line, match, include_local)) {
+            path include_path = string(match[1]);
+            path current_dir = file_path.parent_path();
+            path full_path = current_dir / include_path;
+            ifstream include_file(full_path);
+            if (!include_file.is_open()) {
+                bool found = false;
+                for (const auto& dir : include_directories) {
+                    full_path = dir / include_path;
+                    include_file.open(full_path);
+                    if (include_file.is_open()) {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    cout << "unknown include file " << include_path.string() << " at file " << file_path.string() << " at line " << line_number << endl;
+                    return false;
+                }
+            }
+            int nested_line_number = 1;
+            if (!ProcessIncludes(include_file, output, full_path, include_directories, nested_line_number)) {
+                return false;
+            }
+            include_file.close();
+        } else if (regex_match(line, match, include_system)) {
+            path include_path = string(match[1]);
+            bool found = false;
+            ifstream include_file;
+            for (const auto& dir : include_directories) {
+                path full_path = dir / include_path;
+                include_file.open(full_path);
+                if (include_file.is_open()) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                cout << "unknown include file " << include_path.string() << " at file " << file_path.string() << " at line " << line_number << endl;
+                return false;
+            }
+            int nested_line_number = 1;
+            if (!ProcessIncludes(include_file, output, include_path, include_directories, nested_line_number)) {
+                return false;
+            }
+            include_file.close();
+        } else {
+            output << line << "\n";
+        }
+        line_number++;
+    }
+    return true;
+}
 
 string GetFileContents(string file) {
     ifstream stream(file);
